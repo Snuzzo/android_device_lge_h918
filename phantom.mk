112a77451a6468375d3d110dbc7e622d417d58ad
Snuzzo <jrracinfan@gmail.com>
2017-09-18 11:25:08 -0400

    Lineage -> Phantom

--- phantom.mk
+++ phantom.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/h918/full_h918.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/phantom/common.mk)
 
-# Overlays (inherit after vendor/cm to ensure we override it)
+# Overlays (inherit after vendor to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := lineage_h918
+PRODUCT_NAME := phantom_h918
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="v20" \
